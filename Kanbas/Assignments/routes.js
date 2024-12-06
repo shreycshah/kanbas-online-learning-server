2c558c9e1706e910c3db2e8945f3ac1955ce3beb
shreycshah <shreycshah@gmail.com>
2024-12-05 23:42:14 -0500

    fixed assignments apis

--- Kanbas/Assignments/routes.js
+++ Kanbas/Assignments/routes.js
@@ -1,14 +1,14 @@
 import * as assignmentDao from "./dao.js";
 export default function AssignmentRoutes(app) {
-    app.delete("/api/assignments/:assignmentId", (req, res) => {
+    app.delete("/api/assignments/:assignmentId", async(req, res) => {
         const { assignmentId } = req.params;
-        const status = assignmentDao.deleteAssignment(assignmentId);
+        const status = await assignmentDao.deleteAssignment(assignmentId);
         res.send(status);
     });
-    app.put("/api/assignments/:assignmentId", (req, res) => {
+    app.put("/api/assignments/:assignmentId", async(req, res) => {
         const { assignmentId } = req.params;
         const assignmentUpdates = req.body;
-        const status = assignmentDao.updateAssignment(assignmentId, assignmentUpdates);
+        const status = await assignmentDao.updateAssignment(assignmentId, assignmentUpdates);
         res.send(status);
     });
 }
--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -36,19 +36,19 @@ export default function CourseRoutes(app) {
         const newModule = await modulesDao.createModule(module);
         res.send(newModule);
     });
-    app.get("/api/courses/:courseId/assignments", (req, res) => {
+    app.get("/api/courses/:courseId/assignments", async(req, res) => {
         const { courseId } = req.params;
-        const assignments = assignmentsDao.findAssignmentsForCourse(courseId);
-        console.log("assignments", assignments);
+        console.log("courseId", courseId);
+        const assignments = await assignmentsDao.findAssignmentsForCourse(courseId);
         res.json(assignments);
     });
-    app.post("/api/courses/:courseId/assignments", (req, res) => {
+    app.post("/api/courses/:courseId/assignments", async (req, res) => {
         const { courseId } = req.params;
         const assignment = {
             ...req.body,
             course: courseId,
         };
-        const newAssignment = assignmentsDao.createAssignment(assignment);
+        const newAssignment = await assignmentsDao.createAssignment(assignment);
         res.send(newAssignment);
     });
     app.post("/api/courses", async (req, res) => {
